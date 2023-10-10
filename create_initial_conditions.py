b9993c8b2ac638a5efab987455aa2b0915256369
JoeyKilbride <joeykilbride24@gmail.com>
2023-10-10 16:42:02 +0100

    tidying and Compare2data now a droppy function

--- create_initial_conditions.py
+++ create_initial_conditions.py
@@ -6,7 +6,6 @@ import sys
 #sys.path.insert(0, r'\\NTU-DPM-CTY.ads.ntu.ac.uk\ERD160_projects$\aaaa_Joey\Scripts') # Windows
 sys.path.insert(0, r'/Volumes/ERD160_projects$/aaaa_Joey/Scripts/') # Mac
 print("path:", sys.path)
-import Visualisation as vis
 
 def create_initial_conditions(val, directory):
     """Function for writing dictionary initial conditions for Wray or Masoud 
@@ -92,79 +91,9 @@ for i in range(1):
             eResults['drying_times'] = eResults['drying_times'][RunTimeInputs['sort']]
             eResults['rdx'] = eResults['rdx'][RunTimeInputs['sort']]
 
-        vis.Compare2Data(Results, eResults, cmap_name)
+        dpy.Compare2Data(Results, eResults, cmap_name)
         print("bias angle  = ",Results['bias_angle'])
         print("bias gradient  = ",Results['bias_grad'])
         
     
-# =============================================================================
-
-
-# def Initialise_breath():
-#     """."""
-#     # SVG="5x2 Rectangle"
-#     # directory=r'Z:\aaaa_Joey\Experiments\Kieran\5x2Array'
-# 
-#     # SVG="Optimised_7_Droplet_Hexagon_easy"
-#     # directory=r'Z:\aaaa_Joey\Experiments\PD-7D-H-3'
-#     #CX, CY, Rb, CA = dpy.ReadSVGFile(directory, SVG)
-#     
-#     # CX = np.genfromtxt(os.path.join(directory,'CX.txt'), delimiter=',')/1000
-#     # CY = np.genfromtxt(os.path.join(directory,'CY.txt'), delimiter=',')/1000
-#     # Rb = np.genfromtxt(os.path.join(directory,'Rb.txt'), delimiter=',')/1000
-#     # CA = dpy.genfromtxt(os.path.join(directory,'CA.txt'), delimiter=',')*pi/180
-#     #ca = np.ones(len(cx))*np.pi/2 
-#     
-#          
-#     # CX_unit=cx[touching]-min(cx[touching]) # place bottom corner on origin
-#     # CY_unit=cy[touching]-min(cy[touching]) # place bottom corner on origin
-#     # Rb_unit=r[touching]
-#     # CA_unit=ca[touching] 
-#     # CX=CX_unit
-#     # CY=CY_unit
-#     # Rb=Rb_unit
-#     # CA=CA_unit
-#     # D=1
-#     # i,j=indices((D,D))
-#     # xshift=max(CX)
-#     # yshift=max(CY)
-#     # # mirror unit breath over NXN grid
-#     # print("Mirroring")
-#     # for idx in range(D):
-#     #     for jdx in range(D):
-#     #         print(len(CX_unit))
-#     #         if idx ==0 and jdx ==0:
-#     #             print("Skipping iteration")
-#     #             continue
-#     #         print("Adding more droplets")
-#     #         CX_new=CX_unit + i[idx,jdx]*xshift 
-#     #         CY_new=CY_unit + j[idx,jdx]*yshift
-#     #         CX=np.append(CX,CX_new)
-#     #         CY=np.append(CY,CY_new)
-#     #         Rb=np.append(Rb,Rb_unit)
-#     #         CA=np.append(CA,CA_unit)
-# # =============================================================================
-# #     filter_touching=True
-# #     plt.close('all')
-# #     if filter_touching == True:
-# #         CX, CY, Rb, CA = TouchingCircles(cx,cy,r,ca)
-# #         
-# #     filename = prefix+'_'+str(len(CX))
-# #     RunTimeInputs={} # defines dict
-# #     RunTimeInputs['Rb']=Rb                 # Droplet base radius (metres)
-# #     RunTimeInputs['dt']= dt
-# #     RunTimeInputs['CA']=CA
-# #     RunTimeInputs['xcentres']=CX           # as 1D np array
-# #     RunTimeInputs['ycentres']=CY           # as 1D np array
-# #     RunTimeInputs['DNum']=len(CX)          # total number of droplets in array
-# #     RunTimeInputs['Ambient_RH']=Ambient_RH # Fraction [0-1]
-# #     RunTimeInputs['Ambient_T']=Ambient_T   # Degrees C
-# #     RunTimeInputs['t']=t                   # initial time (s)
-# #     RunTimeInputs['directory']=directory   # where to save data (absolute, no trailing \)
-# #     RunTimeInputs['filename']=filename     # what to call data (no exts)
-# #     RunTimeInputs['Vi'] = GetVolumeCA(RunTimeInputs['CA'],RunTimeInputs['Rb'])
-# # =============================================================================
-#     return
-#    
-# 
 # =============================================================================
